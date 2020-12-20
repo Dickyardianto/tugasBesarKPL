52472316a255c1ea5575f7adbd90260f4e7475cc
dicky Ardianto <dikiardianto19@gmail.com>
2020-12-20 21:26:13 +0700

    first commit

---
+++ src/main/java/id/wadidau/floatingpointvariables/FloatingPointVariablesApplication.java
@@ -0,0 +1,13 @@
+package id.wadidau.floatingpointvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatingPointVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloatingPointVariablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/wadidau/floatingpointvariables/FloatingPointVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package id.wadidau.floatingpointvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatingPointVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
